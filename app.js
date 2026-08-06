1811b0ceaec920c00929569b897638cc60711505
Matheus Giovani da Silva Pereira <matheusgiovani633@gmail.com>
2026-08-05 21:59:37 -0300

    segundo Commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const http = require('http')
+const servidor = http.createServer((req,res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain;charset=utf-8')
+    res.end('Servidor rodando')
+})
+
+servidor.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000')
+})
+
+const express = require('express')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+const flash = require('express-flash')
+
+
+const app = express();
+
+app.use(
+    express.urlencoded({
+        extended: true
+    }),
+)
+app.use(express.json())
+
+
+
+//css
+app.use(express.static('public'))
+
+app.use((req,res,next) => {
+    if(req.session.userid){ 
+        res.locals.session = req.session
+    }
+    next()
+})
